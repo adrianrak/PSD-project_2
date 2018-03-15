00068fb712f90e6257b029938dd2654e425d7df3
Adrian Rak <adrrak@tlen.pl>
2018-03-15 22:02:14 +0100

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+
+    // Project configuration
+    grunt.initConfig({
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'css/style.css': 'scss/style.scss'
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['scss/*.scss'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false,
+                },
+            }
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        'css/*.css',
+                        './*.html'
+                    ]
+                },
+                options: {
+                    server: {
+                        baseDir: "./"
+                    }
+                }
+            }
+        }
+
+    });
+
+    //Load the plugin task
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
+
+    // Default task
+    grunt.registerTask('default', ['sass', 'watch', 'browserSync']);
+}
